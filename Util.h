4a1d847ee7f2c87c11790d9a1e88d5460f06c6f9
Johan Talboom <borfje@gmail.com>
2013-08-19 05:04:25 +0000

    Added hashing macro

--- Util.h
+++ Util.h
@@ -2,6 +2,7 @@
 
 #include <glm/glm.hpp>
 #include <string>
+#include <string.h>
 
 namespace blib
 {
@@ -10,6 +11,16 @@ namespace blib
 		glm::vec2 fromAngle(float angle);
 
 		std::string toString(int number);
+
+
+#define H1(s,i,x)   (x*65599u+(uint8_t)s[(i)<strlen(s)?strlen(s)-1-(i):strlen(s)])
+#define H4(s,i,x)   H1(s,i,H1(s,i+1,H1(s,i+2,H1(s,i+3,x))))
+#define H16(s,i,x)  H4(s,i,H4(s,i+4,H4(s,i+8,H4(s,i+12,x))))
+#define H64(s,i,x)  H16(s,i,H16(s,i+16,H16(s,i+32,H16(s,i+48,x))))
+#define H256(s,i,x) H64(s,i,H64(s,i+64,H64(s,i+128,H64(s,i+192,x))))
+
+#define HASH(s)    ((uint32_t)(H256(s,0,0)^(H256(s,0,0)>>16)))
+
 
 	}
 }
