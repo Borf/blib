52fadf6c90e6ca03df84315b443d929deaaa9b79
Johan Talboom <borfje@gmail.com>
2013-11-05 15:51:04 +0000

    Added start for a particle system / tester

---
+++ blib/ParticleSystem.cpp
@@ -0,0 +1,18 @@
+#include <blib/ParticleSystem.h>
+
+
+blib::ParticleSystem::ParticleSystem( Renderer* renderer )
+{
+	this->renderer = renderer;
+}
+
+void blib::ParticleSystem::update( double elapsedTime )
+{
+
+}
+
+void blib::ParticleSystem::draw()
+{
+
+}
+
---
+++ blib/ParticleSystem.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <string>
+#include <vector>
+
+namespace blib
+{
+	class Renderer;
+
+
+	class ParticleBase
+	{
+	public:
+		glm::vec2 position;
+		glm::vec2 prevPosition;
+		float life;
+
+
+		ParticleBase()
+		{
+			life = 1;
+		}
+	};
+
+	class Emitter
+	{
+
+	};
+
+	class Attractor
+	{
+
+	};
+
+	class EmitterTemplate
+	{
+	public:
+		enum ParticleType
+		{
+				Fading
+		} particleType;
+		std::string texture;
+
+		enum TextureOrder
+		{
+			Random,
+		} textureOrder;
+
+		enum BlendMode
+		{
+			Alpha,
+			Add
+		} blendMode;
+
+		enum Shape
+		{
+			Point,
+			Line,
+			Circle
+		} shape;
+
+		glm::vec2 gravity;
+		bool collision;
+
+		struct ParticleProps
+		{
+			float directionMin;
+			float directionMax; 
+
+			float speedMin;
+			float speedMax;
+
+			float rotationMin;
+			float rotationMax;
+
+			float friction;
+			float fadeSpeedMin;
+			float fadeSpeedMax;
+
+			std::vector<float> size;
+			std::vector<glm::vec4> colors;
+		} particleProps;
+	};
+
+	class ParticleSystem
+	{
+		Renderer* renderer;
+	public:
+		ParticleSystem(Renderer* renderer);
+		void update(double elapsedTime);
+		void draw();
+
+
+		void clear();
+		Emitter* addEmitter(std::string name);
+		void removeEmitter(Emitter* emitter);
+
+		Attractor* addAttractor();
+		void removeAttractor();
+
+	};
+
+
+}
