77bff1ae011ff54fb32bde3c4cb92927383850e5
Johan Talboom <borfje@gmail.com>
2013-11-25 14:34:48 +0000

    Added window.h for linux

---
+++ blib/platform/linux/window.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <blib/IWindow.h>
+
+
+
+
+namespace blib
+{
+	namespace platform
+	{
+		namespace linux
+		{
+			class Window : public blib::IWindow
+			{
+			private:
+				int mouseButtons;
+				std::vector<long> clicks;
+				int lastButton;
+			protected:
+
+			public:
+				Window();
+				virtual ~Window();
+				virtual void create(int icon, std::string title);
+			};
+		}
+	}
+}
