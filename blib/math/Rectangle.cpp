42b3f766e9c2c220284ecc18419cf5034b98cf3c
Johan Talboom <borfje@gmail.com>
2017-01-04 20:29:49 +0100

    Change FBO to RGBA Rectangle contains now include bounds

--- blib/math/Rectangle.cpp
+++ blib/math/Rectangle.cpp
@@ -60,7 +60,7 @@ namespace blib
 
 		bool Rectangle::contains(const glm::vec2 &point) const
 		{
-			return point.x > topleft.x && point.y > topleft.y && point.x < bottomright.x && point.y < bottomright.y;
+			return point.x >= topleft.x && point.y >= topleft.y && point.x <= bottomright.x && point.y <= bottomright.y;
 		}		
 
 		bool Rectangle::containsX(const glm::vec2 &point) const
