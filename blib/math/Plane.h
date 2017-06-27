6d1dd7b97dd6011a20cc69026e59dbd9a147172e
Johan Talboom <borfje@gmail.com>
2017-06-27 21:29:32 +0200

    Added constructor for plane

--- blib/math/Plane.h
+++ blib/math/Plane.h
@@ -10,8 +10,18 @@ namespace blib
 		class Plane
 		{
 		public:
-			glm::vec3 normal;
+			Plane()
+			{
+				D = 0;
+			}
 
+			Plane(const glm::vec3 &normal, float d)
+			{
+				this->normal = normal;
+				this->D = d;
+			}
+
+			glm::vec3 normal;
 			float D;
 		};
 	}
