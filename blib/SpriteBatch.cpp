e3d507eb60272bb37e72ca0c23e296220a6163e6
Johan Talboom <borfje@gmail.com>
2018-03-09 16:16:33 +0100

    IOS error

--- blib/SpriteBatch.cpp
+++ blib/SpriteBatch.cpp
@@ -251,8 +251,8 @@ namespace blib
               //  if (text.find(space, i+1) != std::string::npos || y != cursor.y)
 				{
 					str word = text.substr(i);
-					if (word.find(" ") != std::string::npos)
-						word = word.substr(0, word.find(" "));
+					if (word.find(space) != std::string::npos)
+						word = word.substr(0, word.find(space));
 //					int start = i > 0 && text.rfind(space, i - 1) != std::string::npos ? text.rfind(space, i - 1) + 1 : 0;
 //					str word = text.substr(start, text.find(space, i+1) - start);
 					int wordLength = 0;
